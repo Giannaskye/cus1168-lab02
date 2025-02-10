3b8b363cf92add1e1cf0f870b85c116a830cd7d9
Giannaskye <gianna.sheppard22@my.stjohns.edu>
2025-02-10 15:22:57 -0500

    part 1 Vehicle.java

--- src/main/java/academy/javapro/Vehicle.java
+++ src/main/java/academy/javapro/Vehicle.java
@@ -7,8 +7,42 @@ public abstract class Vehicle {
     protected boolean isRunning;
 
     // TODO: Add constructor
-
+    public Vehicle(make,model,year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
     // TODO: Add abstract methods
+    public abstract void startEngine();
+    public abstract void stopEngine();
+    public abstract void brake();
+    public abstract void accelerate();
+    // TODO: Add getters/setter
+        public Vehicle getMake(){
+            return this.make;
+        }
+        public Vehicle getModel(){
+            return this.model;
+        }
+        public Vehicle getYear(){
+            return this.year;
+        }
+        public Vehicle isRunning(){
+            return false;
+        }
+    
+    public void setMake(Vechicle make){
+        this.make = make;
+    }
+    public void setModel(Vehicle model){
+            this.model = model;
+    }
+    public void setYear(Vehicle year){
+        this.year = year;
+    }
+    public void setIsRunning(isRunning){
+        this.isRunning = isRunning;
 
-    // TODO: Add getters/setters
+    }
+        
 }

c18c18e94f1ce93ece61f1e00954de1c8e262c72
Giannaskye <gianna.sheppard22@my.stjohns.edu>
2025-02-10 16:23:09 -0500

    Part 3 Tesla.java

--- src/main/java/academy/javapro/Tesla.java
+++ src/main/java/academy/javapro/Tesla.java
@@ -5,9 +5,49 @@ public class Tesla extends Vehicle implements Electric, Autonomous {
     private boolean charging;
 
     // TODO: Create constructor that takes model and year
+   public void  Car(model,year){
+        this.model = model;
+        this.year = year;
+   }
     // TODO: Set make to "Tesla"
+   setMake(Vehicle Tesla);
     // TODO: Initialize autopilotEnabled and charging to false
-
+autopilotEnabled = false;
+charging = false;
     // TODO: Implement all required methods from Vehicle, Electric, and Autonomous
     // Each method should include appropriate print statements
+//basic vehicle functions 
+Vehicle startEngine(){
+    if( startEngine==true){
+    System.out.println( make + model +" started");
+}else{
+         System.out.println( make + model +" stopped");
+    }    
+   Vehicle acclerate(){
+        System.out.println( make + model + " is accelerating...");
+   }
+Vehicle brake(){
+    System.out.println( make + model +" is braking...");
 }
+Vehicle isRunning(){
+    System.out.println("Running: "+ getisRunning())"
+// testing autonomous features
+Autonomous enableAutoPilot(){
+    if (enableAutoPilot == true){
+    System.out.println("Autopilot is enabled");
+    }else{
+   System.out.println("Autopilot is disabled");
+}
+}    
+// testing eletric functions 
+Electric charging(){
+    if (charging == true){
+    System.out.println(make + model + "is now charging");
+    System.out.println("Charging Status: " );
+        return charging();
+    else{
+        System.out.println(make + model + "is not charging");
+        System.out.println("Charging Status: " );
+        return charging();
+
+
